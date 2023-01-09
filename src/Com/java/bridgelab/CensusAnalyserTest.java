a827d80520f0caf7c534f1fb0b3f24495b5b5c90
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2023-01-09 09:09:42 +0530

    Added:logic for Loding the Indian State Census Information from a csv file

---
+++ src/Com/java/bridgelab/CensusAnalyserTest.java
@@ -0,0 +1,20 @@
+package Com.java.bridgelab;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
+public class CensusAnalyserTest {
+	private static final String INDIA_CENSUS_CSV_FILE_PATH = "C:\\Users\\Rajani daddimani\\Desktop\\java basic porgrams\\Indian_Census_Anlyser\\IndiaStateCensusData.csv";
+
+    @Test
+    public void givenIndianCensusCSVFileReturnsCorrectRecords() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            int numOfRecords = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
+            Assert.assertEquals(29,numOfRecords);
+        } catch (CensusAnalyserException e) { }
+    }
+
+}
